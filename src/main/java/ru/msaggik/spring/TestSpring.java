7b866a763f1893f1a18bfa262e7aa11825479abc
MSagGik <108148690+MSagGik@users.noreply.github.com>
2022-08-08 15:51:50 +0300

    Update TestSpring.java

--- src/main/java/ru/msaggik/spring/TestSpring.java
+++ src/main/java/ru/msaggik/spring/TestSpring.java
@@ -1,1 +1,20 @@
-package ru.msaggik.spring;import org.springframework.context.support.ClassPathXmlApplicationContext;public class TestSpring {    public static void main(String[] args) {        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(                "applicationContext.xml"        );        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);        musicPlayer.playMusicList();        System.out.println(musicPlayer.getName());        System.out.println(musicPlayer.getVolume());        context.close();    }}
+package ru.msaggik.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        musicPlayer.playMusicList();
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+ 
+        context.close();
+    }
+}
