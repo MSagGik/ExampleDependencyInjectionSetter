34aacbff64d62fad84873fa835a0d8b8febb17fe
MSagGik <Sagaciyang@yandex.ru>
2022-08-08 14:41:43 +0300

    Commit 080822

---
+++ src/main/java/ru/msaggik/spring/MusicPlayer.java
@@ -0,0 +1,36 @@
+package ru.msaggik.spring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+    // создание дополнительных полей с сеттарами и геттарами
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public void playMusicList() {
+        for(Music music: musicList)
+            System.out.println("Playing: " + music.getSong());
+    }
+}
